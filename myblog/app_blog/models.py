8a705103ed7d272fd2683643d278b1cf7c89f776
mysthade <nagiba.moloduy@gmail.com>
2025-04-29 20:41:46 +0300

    Практична робота 5

--- myblog/app_blog/models.py
+++ myblog/app_blog/models.py
@@ -3,8 +3,10 @@ from django.db import models
 from django.urls import reverse
 
 class Category(models.Model):
-    category = models.CharField(u'Категорія', max_length=250)
+    category = models.CharField(u'Категорія',
+                                max_length=250, help_text=u'Максимум 250 сим.')
     slug = models.SlugField(u'Слаг')
+    objects = models.Manager()
 
     class Meta:
         verbose_name = u'Категорія для публікації'
@@ -13,33 +15,56 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
+    def get_absolute_url(self):
+        try:
+            url = reverse('articles-category-list',
+                          kwargs={'slug': self.slug})
+        except:
+            url = "/"
+        return url
+
+
+# Модель для публікацій блогу
 class Article(models.Model):
-    title = models.CharField(u'Заголовок', max_length=250)
-    description = models.TextField(blank=True, verbose_name=u'Опис')
-    pub_date = models.DateTimeField(u'Дата публікації', default=timezone.now)
-    slug = models.SlugField(u'Слаг', unique_for_date='pub_date')
-    main_page = models.BooleanField(u'Головна', default=False)
-    category = models.ForeignKey(Category, related_name='articles', blank=True, null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
+    title = models.CharField('Заголовок', max_length=250)
+    description = models.TextField(blank=True, verbose_name='Опис')
+    pub_date = models.DateTimeField('Дата публікації', default=timezone.now)
+    slug = models.SlugField('Слаг', unique_for_date='pub_date')
+    main_page = models.BooleanField('Головна', default=False)
+    category = models.ForeignKey(
+        Category, 
+        related_name='articles', 
+        blank=True, 
+        null=True, 
+        verbose_name='Категорія', 
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         ordering = ['-pub_date']
-        verbose_name = u'Публікація'
-        verbose_name_plural = u'Публікації'
+        verbose_name = 'Публікація'
+        verbose_name_plural = 'Публікації'
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('news-detail', kwargs={'year': self.pub_date.year, 'month': self.pub_date.month, 'day': self.pub_date.day, 'slug': self.slug})
+        return reverse('news-detail', kwargs={
+            'year': self.pub_date.year,
+            'month': self.pub_date.month,
+            'day': self.pub_date.day,
+            'slug': self.slug
+        })
 
+# Модель для зображень, що пов'язані з публікацією
 class ArticleImage(models.Model):
     article = models.ForeignKey(Article, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(u'Фото', upload_to='photos')
-    title = models.CharField(u'Заголовок', max_length=250, blank=True)
+    image = models.ImageField('Фото', upload_to='photos')
+    title = models.CharField('Заголовок', max_length=250, blank=True)
 
     class Meta:
-        verbose_name = u'Фото для статті'
-        verbose_name_plural = u'Фото для статті'
+        verbose_name = 'Фото для статті'
+        verbose_name_plural = 'Фото для статті'
 
     def __str__(self):
         return self.title
