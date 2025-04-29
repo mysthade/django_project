d90d0f6468c5de61377317a45f7a50ddae45b575
mysthade <nagiba.moloduy@gmail.com>
2025-04-29 19:56:38 +0300

    Практична робота 4

--- myblog/app_blog/admin.py
+++ myblog/app_blog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Category, Article, ArticleImage
 
-# Register your models here.
+# Клас для налаштування вигляду категорій в адмінці
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'slug')
+    prepopulated_fields = {'slug': ('category',)}  # Автоматичне створення слага з категорії
+
+admin.site.register(Category, CategoryAdmin)
+
+# Клас для налаштування вигляду зображень статей в адмінці
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    extra = 1  # Дозволяє додавати додаткові зображення до публікації
+    fields = ('image', 'title')  # Поля, які будуть доступні для редагування в адмінці
+
+# Клас для налаштування вигляду статей в адмінці
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'slug', 'main_page')
+    list_filter = ('category', 'main_page')  # Додано фільтрацію за категорією та головною сторінкою
+    search_fields = ('title', 'description')  # Додано пошук по заголовку та опису
+    prepopulated_fields = {'slug': ('title',)}  # Автоматичне створення слага з заголовку
+    raw_id_fields = ('category',)  # Використовуємо raw_id_field для полегшення вибору категорії
+    inlines = [ArticleImageInline]  # Додаємо вбудовані зображення для статей
+
+admin.site.register(Article, ArticleAdmin)
+
+# Реєстрація ArticleImage для можливості редагування зображень в адмінці
+class ArticleImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'article', 'image')  # Відображаємо назву, статтю та зображення
+    search_fields = ('title',)  # Додаємо пошук по заголовку
+
+admin.site.register(ArticleImage, ArticleImageAdmin)
--- myblog/app_blog/models.py
+++ myblog/app_blog/models.py
@@ -1,3 +1,45 @@
+from django.utils import timezone
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(u'Категорія', max_length=250)
+    slug = models.SlugField(u'Слаг')
+
+    class Meta:
+        verbose_name = u'Категорія для публікації'
+        verbose_name_plural = u'Категорії для публікацій'
+
+    def __str__(self):
+        return self.category
+
+class Article(models.Model):
+    title = models.CharField(u'Заголовок', max_length=250)
+    description = models.TextField(blank=True, verbose_name=u'Опис')
+    pub_date = models.DateTimeField(u'Дата публікації', default=timezone.now)
+    slug = models.SlugField(u'Слаг', unique_for_date='pub_date')
+    main_page = models.BooleanField(u'Головна', default=False)
+    category = models.ForeignKey(Category, related_name='articles', blank=True, null=True, verbose_name=u'Категорія', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = u'Публікація'
+        verbose_name_plural = u'Публікації'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news-detail', kwargs={'year': self.pub_date.year, 'month': self.pub_date.month, 'day': self.pub_date.day, 'slug': self.slug})
+
+class ArticleImage(models.Model):
+    article = models.ForeignKey(Article, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(u'Фото', upload_to='photos')
+    title = models.CharField(u'Заголовок', max_length=250, blank=True)
+
+    class Meta:
+        verbose_name = u'Фото для статті'
+        verbose_name_plural = u'Фото для статті'
+
+    def __str__(self):
+        return self.title
